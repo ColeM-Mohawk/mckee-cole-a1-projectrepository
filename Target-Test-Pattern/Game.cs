8616a8e078c3cd56560542825c856435881097d9
MckeeCMohawk <000983139@mohawkcollege.ca>
2026-02-23 16:59:31 -0500

    MohawkGame2D

---
+++ Target-Test-Pattern/Game.cs
@@ -0,0 +1,47 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Target Pattern");
+            Window.SetSize(400, 400);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.Black);
+
+            Draw.FillColor = Color.Red;
+            for (int count = 0; count < 10; count ++)
+            {
+                bool isEven = count % 2 == 0;
+                if (isEven == true)
+                    Draw.FillColor = Color.Red;
+                else Draw.FillColor = Color.White;
+
+                
+                float radius = 150 * (10 - count) / 10f;
+                Draw.Circle(200, 200, radius);
+            }
+        }
+    }
+
+}
---
+++ functions-test/Game.cs
@@ -0,0 +1,50 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Looking");
+            Window.SetSize(800, 800);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.Black);
+
+            DrawEyeball(250, 400);
+            DrawEyeball(550, 400);
+       
+        }
+
+        Color irisColor = new Color();
+        void DrawEyeball(int x, int y)
+        {
+            // Draw eyeball
+            Draw.LineSize = 2;                //1px outline
+            Draw.LineColor = Color.OffWhite;     // Outline colour
+            Draw.FillColor = Color.White;     // Cornea colour
+            Draw.Circle(x, y, 100);           // Draw cornea
+            Draw.FillColor = Color.Red;     // Pupil colour
+            Draw.Circle(x, y, 45);            //Draw pupul
+        }
+    }
+}
+
